3802d68d073aa2c3c5335edbf99999ad9aa70295
vikasyadav01hp <vikasyadav01.hp@outlook.com>
2024-08-03 11:42:31 +0530

    commit

--- index.js
+++ index.js
@@ -2,14 +2,6 @@
 const person=["vikas", "yadav", "10-07-2003"];
 
 
-// ForEach
-
-// person.forEach(i => {
-//     document.getElementById("Firstname").innerHTML=person[0];
-//     document.getElementById("lastname").innerHTML=person[1];
-//     document.getElementById("dob").innerHTML=person[2];
-// });
-
 for(let i=0;i<3;i++){
     document.getElementById("Firstname").innerHTML=person[0];
     document.getElementById("lastname").innerHTML=person[1];
